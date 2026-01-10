387901563c535ebc79f368bf69d3ed50d5f38bd8
massimocora3-create <massimocora3@gmail.com>
2026-01-10 22:30:43 +0100

    Update data.js

--- programmi-personalizzati/data.js
+++ programmi-personalizzati/data.js
@@ -2,3 +2,6 @@ window.RS_PROGRAMMI = {
   "c2100c63b2bf36cff60fd5c6ccbf6dcfb128400896d4722d67c9c4bddb5182e3": "programmi/cora-massimo.html"
 };
 
+window.RS_PROGRAMMI = {
+  "875d308906bc766b5e0eab6f844940ef8ddd36b2754df8b084d23be8416a8bfe": "programmi/foschi-federico.html"
+};
