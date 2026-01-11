ab23e97651692ecd945b3d52c29adfdc46c3f954
massimocora3-create <massimocora3@gmail.com>
2026-01-11 09:21:48 +0100

    Add new entry for travasoni-beatrice.html

--- programmi-personalizzati/data.js
+++ programmi-personalizzati/data.js
@@ -4,5 +4,6 @@ window.RS_PROGRAMMI = {
   "54f14f2a8e8d242f22bdc97d4ef4ed3abfd1a7c3027f180e0ef34f2474be49ad": "programmi/gilli-costanza.html",
   "eff35831955948096a516ccc85b19d5e603673be01355e7df329ca445ce25a9d": "programmi/pasetti-luca.html",
   "0a11b3ff42c12c6e8da895b49512b459cde6322b35d572ecfae0b3e91007f658": "programmi/brugnara-ilenia.html",
-  "707035a06b7cff7deefc04a12da4f429068ca6a321d70717dd923cfbff5a072b": "programmi/mazzini-mauro.html"
+  "707035a06b7cff7deefc04a12da4f429068ca6a321d70717dd923cfbff5a072b": "programmi/mazzini-mauro.html" ,
+  "61f793834b9901590637cb81fec93511f2660a676dc4c9857fe6e2b317ae970b": "programmi/travasoni-beatrice.html" ,
 };
