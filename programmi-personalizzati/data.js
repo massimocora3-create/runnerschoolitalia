e2ee42c539d5207faba42a0901746725e1e84e36
massimocora3-create <massimocora3@gmail.com>
2026-01-11 09:17:21 +0100

    Consolidate RS_PROGRAMMI object entries

--- programmi-personalizzati/data.js
+++ programmi-personalizzati/data.js
@@ -1,21 +1,8 @@
 window.RS_PROGRAMMI = {
-  "c2100c63b2bf36cff60fd5c6ccbf6dcfb128400896d4722d67c9c4bddb5182e3": "programmi/cora-massimo.html"
-};
-
-window.RS_PROGRAMMI = {
-  "875d308906bc766b5e0eab6f844940ef8ddd36b2754df8b084d23be8416a8bfe": "programmi/foschi-federico.html"
-};
-
-window.RS_PROGRAMMI = {
-  "54f14f2a8e8d242f22bdc97d4ef4ed3abfd1a7c3027f180e0ef34f2474be49ad": "programmi/gilli-costanza.html"
-};
-
-window.RS_PROGRAMMI = {
-  "eff35831955948096a516ccc85b19d5e603673be01355e7df329ca445ce25a9d": "programmi/pasetti-luca.html"
-};
-window.RS_PROGRAMMI = {
-  "0a11b3ff42c12c6e8da895b49512b459cde6322b35d572ecfae0b3e91007f658": "programmi/brugnara-ilenia.html"
-};
-window.RS_PROGRAMMI = {
+  "c2100c63b2bf36cff60fd5c6ccbf6dcfb128400896d4722d67c9c4bddb5182e3": "programmi/cora-massimo.html",
+  "875d308906bc766b5e0eab6f844940ef8ddd36b2754df8b084d23be8416a8bfe": "programmi/foschi-federico.html",
+  "54f14f2a8e8d242f22bdc97d4ef4ed3abfd1a7c3027f180e0ef34f2474be49ad": "programmi/gilli-costanza.html",
+  "eff35831955948096a516ccc85b19d5e603673be01355e7df329ca445ce25a9d": "programmi/pasetti-luca.html",
+  "0a11b3ff42c12c6e8da895b49512b459cde6322b35d572ecfae0b3e91007f658": "programmi/brugnara-ilenia.html",
   "707035a06b7cff7deefc04a12da4f429068ca6a321d70717dd923cfbff5a072b": "programmi/mazzini-mauro.html"
 };
