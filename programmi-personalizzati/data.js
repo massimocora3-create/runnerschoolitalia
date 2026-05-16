f219296b361df2c8683ff84eaa859b2385d41b23
massimocora3-create <massimocora3@gmail.com>
2026-05-16 14:07:15 +0200

    Add new entry for 'bissa-federico.html' in data.js

--- programmi-personalizzati/data.js
+++ programmi-personalizzati/data.js
@@ -50,5 +50,6 @@ window.RS_PROGRAMMI = {
   "9b6d2180912392aabc9521d5bd32744bcad5c3ba0c4ae42ff7abd81a77688c45": "programmi/perlini-emma.html",
   "181292970e2386b946e1061ade209c38e39cb5463258b02364ede97cd8eff797": "programmi/dacunto-paolo.html",
   "1c3016aec9c32fa3f9f18613cdbb4388fe4558320fc95040491025392f09f98b": "programmi/cardi-davide.html",
-  "5458fd000ef8ebc33107b37217aa509aa98847e2d184bfdccacda4d35c92cd2f": "programmi/carli-federica.html"
+  "5458fd000ef8ebc33107b37217aa509aa98847e2d184bfdccacda4d35c92cd2f": "programmi/carli-federica.html",
+  "de5d54e68c7810e50141dc5aaab346f69aafdd598126cbe214c0a76fd9c903ba": "programmi/bissa-federico.html"
   };
