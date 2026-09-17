da9dde00d4942a643feb02d569af1a08ba546014
massimocora3-create <massimocora3@gmail.com>
2026-09-17 17:03:23 +0200

    Add new program entry for Davino Jonatha

--- programmi-personalizzati/data.js
+++ programmi-personalizzati/data.js
@@ -65,5 +65,6 @@ window.RS_PROGRAMMI = {
   "251549d65f294ad3105e5f83c6e774edff7d4b5397c2ff473830a686a6ba3354": "programmi/borgatti-benedetta.html",
   "8eb16419b34432a7d3ec6abcad38362db4d613bb71a744e94ba6089721373e97": "programmi/mazzoni-manuela.html",
   "018685a482daabcc87decbfbc4e48f7c5a2519d5fb239d67a501234d28296e41": "programmi/iepparello-giovanna.html",
-  "c4b9fea93b8a751b92a57f206526e7fbf7882563b9947b4a046bd5e2eec39770": "programmi/saponaro-modesto.html"
+  "c4b9fea93b8a751b92a57f206526e7fbf7882563b9947b4a046bd5e2eec39770": "programmi/saponaro-modesto.html",
+  "85e8d1f8840b064d01b3ec103e8971733bdc08ffc40f6393997a93037fc0a4c5": "programmi/davino-jonatha.html"
   };
